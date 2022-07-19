91e213d3ccf25b3ff2876733da7a317079e6b3b2
s12souma <s12souma@gmail.com>
2022-07-19 14:42:54 +0900

    Add index 2022.07.19

---
+++ test.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+
+my $debug;
+
+binmode(STDOUT,":utf8");
+
+BEGIN{
+
+}
+END{
+
+}
+
+&main;
+#---------------------------
+#主処理
+#＊＊＊＊＊＊＊＊＊＊＊＊
+#＊＊＊＊＊
+#--------------------------
+sub main(){
+    print "test\n";
+    sleep 3;
+}
+
+sub test2(){
+    
+}
+
+
---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#コメントテスト 2022.7.6 
+import time
+
+def main():
+    #print(test1())
+    print("test")
+    time.sleep(3)
+
+def test1():
+    return True
+
+def test2():
+    return True
+
+if __name__ == "__main__":
+    main()
